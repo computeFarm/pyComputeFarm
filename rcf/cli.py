8eb0a2d9d53e407cdd53c55e55a4fa7df02c0baf
Stephen Gaito <stephen@perceptisys.co.uk>
2022-11-21 17:06:35 +0000

    rcf setup now works

--- rcf/cli.py
+++ rcf/cli.py
@@ -9,13 +9,21 @@ import rcf.cryption
 import rcf.config
 
 @click.group()
-@click.option('-c', '--config', default='config',
-  help="Configuration directory path (default: 'config')"
+@click.option('-c', '--config', default='config', show_default=True,
+  help="Configuration directory path"
+)
+@click.option('-s', '--secrets', default=False, show_default=True,
+  is_flag=True, help="Show the secrets"
+)
+@click.option('-v', '--verbose', default=False, show_default=True,
+  is_flag=True, help="Be verbose"
 )
 @click.pass_context
-def cli(ctx, config) :
+def cli(ctx, config, secrets, verbose) :
   ctx.ensure_object(dict)
   ctx.obj['configPath'] = config
+  ctx.obj['secrets']    = secrets
+  ctx.obj['verbose']    = verbose
 
 cli.add_command(rcf.setup.setup)
 cli.add_command(rcf.run.run)
--- rcf/setup.py
+++ rcf/setup.py
@@ -1,9 +1,12 @@
 
 import click
+import datetime
 import importlib.resources
 import jinja2
 from pathlib import Path
+import pexpect
 import platform
+import os
 import sys
 import tempfile
 import yaml
@@ -29,7 +32,7 @@ def copyFile(fileContents, toPath) :
   with open(toPath, 'w') as toFile :
     toFile.write(fileContents)
 
-def jinjaFile(fromTemplate, toPath, config, secrets) :
+def jinjaFile(fromTemplate, toPath, config, secrets, logFile) :
   env = {}
   rcf.config.mergeYamlData(env, config,  '.')
   rcf.config.mergeYamlData(env, secrets, '.')
@@ -47,7 +50,7 @@ def jinjaFile(fromTemplate, toPath, config, secrets) :
     print(yaml.dump(env))
     print("==========================================================")
 
-def createRunCommandFor(aRole, rCmds, rVars, aDir, config, secrets) :
+def createRunCommandFor(aRole, rCmds, rVars, aHost, aDir, config, secrets, logFile) :
   cmdTemplate = """#!/bin/sh
 
   {% for aCmd in rCmds %}
@@ -67,16 +70,37 @@ def createRunCommandFor(aRole, rCmds, rVars, aDir, config, secrets) :
     for aKey, aValue in aCmd.items() :
       aCmd[aKey] = aValue.format_map(rVars)
   config['rCmds'] = rCmds
-  jinjaFile(cmdTemplate, theTargetFile, config, secrets)
+  jinjaFile(cmdTemplate, theTargetFile, config, secrets, logFile)
   config['rCmds'] = None
   theTargetFile.chmod(0o0755)
 
-def createFilesFor(aRole, rFiles, rVars, aDir, config, secrets) :
+def runCommandFor(aRole, rVars, aHost, config, secrets, logFile) :
+  runCmdPath = os.path.join("{pcfHome}".format_map(rVars), "tmp", 'run_command_'+aRole)
+  runCmd = pexpect.spawn(
+    "ssh {ssh_user}@{aHost} {runCmdPath}".format(
+      aRole=aRole,
+      aHost=aHost,
+      runCmdPath=runCmdPath,
+      ssh_user=config['ssh_user']
+    ),
+    encoding='utf-8'
+  )
+  runCmd.logfile_read = logFile
+  while True :
+    pResult = runCmd.expect([
+      "Enter passphrase for key", pexpect.EOF, pexpect.TIMEOUT
+    ])
+    if pResult == 0 :
+      runCmd.sendline(secrets['ssh_pass'])
+    else : break
+
+def createFilesFor(aRole, rFiles, rVars, aHost, aDir, config, secrets, logFile) :
+  logFile.write(f"creating files for {aRole} on {aHost}\n")
   for aFile in rFiles :
     contents = loadResourceFor(aRole, aFile['src'])
     theTargetFile = aDir / aFile['dest'].format_map(rVars)
     if aFile['src'].endswith('.j2') :
-      jinjaFile(contents, theTargetFile, config, secrets)
+      jinjaFile(contents, theTargetFile, config, secrets, logFile)
     else :
       copyFile(contents, theTargetFile)
     theTargetMode = 0o0644
@@ -84,7 +108,8 @@ def createFilesFor(aRole, rFiles, rVars, aDir, config, secrets) :
       theTargetMode = aFile['mode']
     theTargetFile.chmod(theTargetMode)
 
-def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets) :
+def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets, logFile) :
+  logFile.write(f"creating global resources for {aRole} on {aHost}\n")
   rTasks = loadTasksFor(aRole)
   rVars  = {}
   mergeVars(rVars, gVars)
@@ -97,45 +122,92 @@ def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets) :
       theTargetDir.mkdir(parents=True, exist_ok=True)
 
   if 'files' in rTasks :
-    createFilesFor(aRole, rTasks['files'], rVars, aDir, config, secrets)
+    createFilesFor(aRole, rTasks['files'], rVars, aHost,  aDir, config, secrets, logFile)
 
   if 'commands' in rTasks :
-    createRunCommandFor(aRole, rTasks['commands'], rVars, aDir, config, secrets)
+    createRunCommandFor(aRole, rTasks['commands'], rVars, aHost, aDir, config, secrets, logFile)
 
   if 'workers' in config :
     if aRole in config['workers'] :
       if 'list' in rTasks and 'list' in config['workers'][aRole] :
         for aWorker in config['workers'][aRole]['list'] :
           rVars['aWorker'] = aWorker
-          createFilesFor(aRole, rTasks['list'], rVars, aDir, config, secrets)
+          createFilesFor(aRole, rTasks['list'], rVars, aHost, aDir, config, secrets, logFile)
 
-def createLocalResources(setupConfig, config, secrets) :
-  setupConfig['tmpDir'] = tempfile.mkdtemp(prefix='rcf-')
-  tmpDir = Path(setupConfig['tmpDir'])
-  gConfig = config['globalConfig']
+def runCommandsFor(aRole, gVars, aHost, config, secrets, logFile) :
+  logFile.write(f"creating lobal resources for {aRole} on {aHost}\n")
+  rTasks = loadTasksFor(aRole)
+  rVars  = {}
+  mergeVars(rVars, gVars)
+  if 'vars' in rTasks :
+    mergeVars(rVars, rTasks['vars'])
+  if 'commands' in rTasks :
+    runCommandFor(aRole, rVars, aHost, config, secrets, logFile)
 
-  gTasks  = loadTasksFor()
-  gVars   = {}
-  if 'vars' in gTasks :
-    mergeVars(gVars, gTasks['vars'])
+def createLocalResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
+  logFile.write(f"creating local resources for {aHost}\n")
+  hDir = tmpDir / aHost
+  hDir.mkdir(parents=True, exist_ok=True)
 
-  hList   = gConfig['hostList']
-  for aHost in hList :
-    hConfig = config[aHost]
-    hDir = tmpDir / aHost
-    hDir.mkdir()
-    if aHost == platform.node() :
+  if aHost == platform.node() :
+    createLocalResourcesFor(
+      'taskManager', gVars, aHost, hDir, config, secrets, logFile
+    )
+  if 'workers' in config :
+    createLocalResourcesFor(
+      'allWorkers', gVars, aHost, hDir, config, secrets, logFile
+    )
+    for aWorker in config['workers'] :
       createLocalResourcesFor(
-        'taskManager', gVars, aHost, hDir, hConfig, secrets[aHost]
+        aWorker, gVars, aHost, hDir, config, secrets, logFile
       )
-    if 'workers' in hConfig :
-      createLocalResourcesFor(
-        'allWorkers', gVars, aHost, hDir, hConfig, secrets[aHost]
-      )
-      for aWorker in hConfig['workers'] :
-        createLocalResourcesFor(
-          aWorker, gVars, aHost, hDir, hConfig, secrets[aHost]
-        )
+
+def rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
+    logFile.write(f"rsyncing resouces to {aHost}\n")
+    rsyncCmd = pexpect.spawn(
+      "rsync -av {tmpDir}/{aHost}/ {ssh_user}@{aHost}:.".format(
+        tmpDir=tmpDir,
+        aHost=aHost,
+        ssh_user=config['ssh_user']
+      ),
+      encoding='utf-8'
+    )
+    rsyncCmd.logfile_read = logFile
+    while True :
+      pResult = rsyncCmd.expect([
+        "Enter passphrase for key", pexpect.EOF, pexpect.TIMEOUT
+      ])
+      if pResult == 0 :
+        rsyncCmd.sendline(secrets['ssh_pass'])
+      else : break
+
+def runCommandsForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
+  logFile.write(f"running commands on {aHost}\n")
+  if aHost == platform.node() :
+    runCommandsFor('taskManager', gVars, aHost, config, secrets, logFile)
+  if 'workers' in config :
+    runCommandsFor('allWorkers', gVars, aHost, config, secrets, logFile)
+    for aWorker in config['workers'] :
+      runCommandsFor(aWorker, gVars, aHost, config, secrets, logFile)
+
+def setupAHost(tmpDir, aHost, gVars, config, secrets) :
+  print(f"Setting up host {aHost} in {tmpDir}/{aHost}")
+  timeNow = datetime.datetime.now()
+  logFilePath = Path("logs") / (aHost + '-' + timeNow.strftime('%Y-%m-%d_%H-%M-%s.%f'))
+  logFilePath.parent.mkdir(parents=True, exist_ok=True)
+  with open(logFilePath, "w") as logFile :
+    createLocalResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+    rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+    runCommandsForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+
+def isHostUp(aHost) :
+  pingCmd = pexpect.spawn(f"ping {aHost}")
+  pResult = pingCmd.expect_exact([
+    " Destination Host Unreachable",
+    "64 bytes from "
+  ])
+  pingCmd.terminate(force=True)
+  return pResult == 1
 
 @click.command()
 @click.pass_context
@@ -149,15 +221,27 @@ def setup(ctx) :
   config     = rcf.config.initializeConfig(configPath)
   secrets    = rcf.config.initializeSecrets()
   passPhrase = None
-  #if rcf.config.hasVaults(configPath) :
-  #  passPhrase = rcf.config.askForPassPhrase()
+  if rcf.config.hasVaults(configPath) :
+    passPhrase = rcf.config.askForPassPhrase()
   rcf.config.loadGlobalConfiguration(config, secrets, passPhrase=passPhrase)
   rcf.config.loadConfigurationFor(config, secrets, passPhrase=passPhrase)
+  if ctx.obj['verbose'] :
+    print("----------------------------------------------------")
+    print(yaml.dump(config))
+  if ctx.obj['secrets'] :
+    print("----------------------------------------------------")
+    print(yaml.dump(secrets))
   print("----------------------------------------------------")
-  print(yaml.dump(config))
-  print("----------------------------------------------------")
-  #print(yaml.dump(secrets))
-  #print("----------------------------------------------------")
-  setupConfig = {}
-  createLocalResources(setupConfig, config, secrets)
-  print(yaml.dump(setupConfig))
+
+  tmpDir = Path(tempfile.mkdtemp(prefix='rcf-'))
+
+  gConfig = config['globalConfig']
+  gTasks  = loadTasksFor()
+  gVars   = {}
+  if 'vars' in gTasks :
+    mergeVars(gVars, gTasks['vars'])
+
+  hList = gConfig['hostList']
+  for aHost in hList :
+    if isHostUp(aHost) :
+      setupAHost(tmpDir, aHost, gVars, config[aHost], secrets[aHost])
