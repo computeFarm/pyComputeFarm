4cba46a675651bf0454a8540d7456e6570a6af83
Stephen Gaito <stephen@perceptisys.co.uk>
2022-11-22 19:31:03 +0000

    refactored to use threading and processes for setup and run

--- rcf/run.py
+++ rcf/run.py
@@ -1,9 +1,152 @@
 
 import click
+import datetime
+import importlib.resources
+from multiprocessing import Process
+from pathlib import Path
+import pexpect
 import sys
+from threading import Thread
+import time
 import yaml
 
 import rcf.config
+
+# We *could* use an asycnio pattern... however the tasks for each host are
+# fundamentally synchronous and must be done in a specific order. The only
+# asyncio boost we might get would be from co-routines yeilding to
+# co-routines working on a *different* host.
+
+# Unforutnately, ssh and in particular pexpect based ssh is *not*
+# async-able (the `async_=True` option is only a halfway measure and, in
+# particular, the logfiles *must* all be synchronous as pexepct does *not*
+# await any file io).
+
+# This strongly suggests we need to consider either:
+#   1. python multi-threading
+# OR
+#   2. python multi-processing
+
+# We do not have any synchronization issuses *between* the work on any
+# given hosts. Essentially all of the required state is loaded as
+# configuration. For a given host all processing is independent of any
+# other host. So we load the configuration, and start a thread/process
+# going to work on each host and then wait for these threads/processes to
+# complete.
+
+# In particular, none of our work is particularly CPU bound, this suggests
+# the difference between the use of threads vs processes is minimal (for
+# our use) and so we *will* use threads (usually scheduled, in our linux
+# case, by the OS).
+
+timeNow = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%s.%f')
+
+def loadResourceFor(aRole, aResource) :
+  if aRole :
+    contents = importlib.resources.read_text('rcf.roleResources.'+aRole, aResource)
+  else :
+    contents = importlib.resources.read_text('rcf.roleResources', aResource)
+  return contents
+
+def loadTasksFor(aRole=None) :
+  taskYaml = loadResourceFor(aRole, 'tasks.yaml')
+  return yaml.safe_load(taskYaml)
+
+def mergeVars(oldVars, newVars) :
+  for aKey, aValue in newVars.items() :
+    oldVars[aKey] = aValue.format(oldVars)
+
+def runCommandOnHost(aHost, runCmdPath, sshOpts, config, secrets, logFile, timeOut=30) :
+  logFile.write(f"running command [{runCmdPath}] on {aHost}\n")
+  if timeOut is None : logFile.write("running with no timeout!\n")
+  else : logFile.write(f"running with timeOut = {timeOut}\n")
+  runCmd = pexpect.spawn(
+    "ssh {sshOpts} {ssh_user}@{aHost} {runCmdPath}".format(
+      sshOpts=sshOpts,
+      aHost=aHost,
+      runCmdPath=runCmdPath,
+      ssh_user=config['ssh_user'],
+      timeout=timeOut
+    ),
+    encoding='utf-8'
+  )
+  runCmd.logfile_read = logFile
+  while True :
+    pResult = runCmd.expect([
+      "Enter passphrase for key", pexpect.EOF, pexpect.TIMEOUT
+    ], timeout=timeOut)
+    if pResult == 0 :
+      runCmd.sendline(secrets['ssh_pass'])
+    elif pResult == 1 :
+      logFile.write("EOF\n")
+      break
+    elif pResult == 2 :
+      logFile.write("TIMED OUT\n")
+      break
+    else :
+      logFile.write("Unknown return\n")
+      break
+  if not runCmd.eof() : logFile.write(runCmd.read())
+  runCmd.close()
+  logFile.write(f"  exit status: {runCmd.exitstatus}\n")
+  logFile.write(f"signal status: {runCmd.signalstatus}\n")
+  return runCmd.exitstatus
+
+def mountSshfsOnAHost(aHost, gVars, config, secrets, logFileBase) :
+  print(f"Mount sshfs on host {aHost}")
+  runCmdPath = "sshfs -o ssh_command='ssh -o ServerAliveInterval=60 ' -f -d {ssh_user}@{filesHost}:{filesOrig} {filesDest}".format(
+    ssh_user=config['ssh_user'],
+    filesHost=config['files']['host'],
+    filesOrig=config['files']['orig'].format_map(gVars),
+    filesDest=config['files']['dest'].format_map(gVars)
+  )
+  exitStatus = 0
+  with open(str(logFileBase)+'-mount', "w") as logFile :
+    exitStatus = runCommandOnHost(aHost, runCmdPath, "-t -o ServerAliveInterval=60", config, secrets, logFile, timeOut=None)
+  print(f"Mounted sshfs on host {aHost} (exit status: {exitStatus})")
+
+def unmountSshfsOnAHost(mountProcess, aHost, gVars, config, secrets, logFileBase) :
+  print(f"Unmount sshfs on host {aHost}")
+
+  runCmdPath = "fusermount -u {filesDest}".format(
+    filesDest=config['files']['dest'].format_map(gVars)
+  )
+  exitStatus = 0
+  with open(str(logFileBase)+'-unmount', "w") as logFile :
+    exitStatus = runCommandOnHost(aHost, runCmdPath, "-t", config, secrets, logFile)
+  print(f"Unmounted sshfs on host {aHost} (exit status: {exitStatus})")
+  if mountProcess.is_alive() :
+    time.sleep(5)
+    if mountProcess.is_alive() :
+      mountProcess.terminate()
+      if mountProcess.is_alive() :
+        mountProcess.kill()
+      print(f"TERMINATED daemon thread: mounted sshfs on host {aHost}")
+
+def startAHost(aHost, gVars, config, secrets, logFileBase) :
+  print(f"Starting host {aHost}")
+  runCmdPath = Path("{pcfHome}".format_map(gVars)) / 'bin' / 'startComputeFarm'
+  exitStatus = 0
+  with open(str(logFileBase)+'-start', "w") as logFile :
+    exitStatus = runCommandOnHost(aHost, runCmdPath, "", config, secrets, logFile)
+  print(f"Started host {aHost} (exit status:: {exitStatus})")
+
+def stopAHost(aHost, gVars, config, secrets, logFileBase) :
+  print(f"Stopping host {aHost}")
+  runCmdPath = Path("{pcfHome}".format_map(gVars)) / 'bin' / 'stopComputeFarm'
+  exitStatus = 0
+  with open(str(logFileBase)+'-stop', "w") as logFile :
+    exitStatus = runCommandOnHost(aHost, runCmdPath, "", config, secrets, logFile)
+  print(f"Stoped host {aHost} (exit status: {exitStatus})")
+
+def isHostUp(aHost) :
+  pingCmd = pexpect.spawn(f"ping {aHost}")
+  pResult = pingCmd.expect_exact([
+    " Destination Host Unreachable",
+    "64 bytes from "
+  ])
+  pingCmd.terminate(force=True)
+  return pResult == 1
 
 @click.command()
 @click.pass_context
@@ -21,8 +164,54 @@ def run(ctx) :
     passPhrase = rcf.config.askForPassPhrase()
   rcf.config.loadGlobalConfiguration(config, secrets, passPhrase=passPhrase)
   rcf.config.loadConfigurationFor(config, secrets, passPhrase=passPhrase)
+  if ctx.obj['verbose'] :
+    print("----------------------------------------------------")
+    print(yaml.dump(config))
+  if ctx.obj['secrets'] :
+    print("----------------------------------------------------")
+    print(yaml.dump(secrets))
   print("----------------------------------------------------")
-  print(yaml.dump(config))
-  print("----------------------------------------------------")
-  #print(yaml.dump(secrets))
-  #print("----------------------------------------------------")
+
+  gConfig = config['globalConfig']
+  gTasks  = loadTasksFor()
+  gVars   = {}
+  if 'vars' in gTasks :
+    mergeVars(gVars, gTasks['vars'])
+  print(yaml.dump(gTasks))
+  hList = gConfig['hostList']
+  mountProcesses = []
+  unmountThreads = []
+  startThreads   = []
+  stopThreads    = []
+  for aHost in hList :
+    if not isHostUp(aHost) : continue
+
+    logFileBase = Path("logs") / (aHost + '-' + timeNow)
+    logFileBase.parent.mkdir(parents=True, exist_ok=True)
+
+    mountProcess = Process(target=mountSshfsOnAHost, args=[
+      aHost, gVars, config[aHost], secrets[aHost], logFileBase
+    ])
+    mountProcesses.append(mountProcess)
+    unmountThreads.append(Thread(target=unmountSshfsOnAHost, args=[
+      mountProcess, aHost, gVars, config[aHost], secrets[aHost], logFileBase
+    ]))
+    startThreads.append(Thread(target=startAHost, args=[
+      aHost, gVars, config[aHost], secrets[aHost], logFileBase
+    ]))
+    stopThreads.append(Thread(target=stopAHost, args=[
+      aHost, gVars, config[aHost], secrets[aHost], logFileBase
+    ]))
+
+  for aProcess in mountProcesses : aProcess.start()
+  for aThread  in startThreads   : aThread.start()
+  while True :
+    response = input("Type 'stop' to stop the compute farm: ")
+    if response == 'stop' : break
+  for aThread  in stopThreads    : aThread.start()
+  for aThread  in unmountThreads : aThread.start()
+
+  for aThread  in unmountThreads : aThread.join()
+  for aThread  in stopThreads    : aThread.join()
+  for aThread  in startThreads   : aThread.join()
+  for aProcess in mountProcesses : aProcess.join()
--- rcf/setup.py
+++ rcf/setup.py
@@ -9,9 +9,37 @@ import platform
 import os
 import sys
 import tempfile
+from threading import Thread
 import yaml
 
 import rcf.config
+
+# We *could* use an asycnio pattern... however the tasks for each host are
+# fundamentally synchronous and must be done in a specific order. The only
+# asyncio boost we might get would be from co-routines yeilding to
+# co-routines working on a *different* host.
+
+# Unforutnately, ssh and in particular pexpect based ssh is *not*
+# async-able (the `async_=True` option is only a halfway measure and, in
+# particular, the logfiles *must* all be synchronous as pexepct does *not*
+# await any file io).
+
+# This strongly suggests we need to consider either:
+#   1. python multi-threading
+# OR
+#   2. python multi-processing
+
+# We do not have any synchronization issuses *between* the work on any
+# given hosts. Essentially all of the required state is loaded as
+# configuration. For a given host all processing is independent of any
+# other host. So we load the configuration, and start a thread/process
+# going to work on each host and then wait for these threads/processes to
+# complete.
+
+# In particular, none of our work is particularly CPU bound, this suggests
+# the difference between the use of threads vs processes is minimal (for
+# our use) and so we *will* use threads (usually scheduled, in our linux
+# case, by the OS).
 
 def loadResourceFor(aRole, aResource) :
   if aRole :
@@ -50,22 +78,24 @@ def jinjaFile(fromTemplate, toPath, config, secrets, logFile) :
     print(yaml.dump(env))
     print("==========================================================")
 
-def createRunCommandFor(aRole, rCmds, rVars, aHost, aDir, config, secrets, logFile) :
+def createRunCommandFor(
+  aRole, rCmds, rVars, theTargetFile,
+  aHost, aDir, config, secrets, logFile
+) :
   cmdTemplate = """#!/bin/sh
 
   {% for aCmd in rCmds %}
   echo ""
   echo "---------------------------------------------------------"
-  echo Running command {{ aCmd.name }}
+  {% if aCmd.name %}echo Running command {{ aCmd.name }}{% endif %}
   echo ""
   echo "cmd: [{{ aCmd.cmd }}]"
   echo "---------------------------------------------------------"
-  cd {{ aCmd.chdir }}
+  {% if aCmd.chdir %}cd {{ aCmd.chdir }}{% endif %}
   {{ aCmd.cmd }}
   echo "---------------------------------------------------------"
   {% endfor %}
 """
-  theTargetFile = aDir / "{pcfHome}".format_map(rVars) / 'tmp' / ('run_command_'+aRole)
   for aCmd in rCmds :
     for aKey, aValue in aCmd.items() :
       aCmd[aKey] = aValue.format_map(rVars)
@@ -73,26 +103,6 @@ def createRunCommandFor(aRole, rCmds, rVars, aHost, aDir, config, secrets, logFi
   jinjaFile(cmdTemplate, theTargetFile, config, secrets, logFile)
   config['rCmds'] = None
   theTargetFile.chmod(0o0755)
-
-def runCommandFor(aRole, rVars, aHost, config, secrets, logFile) :
-  runCmdPath = os.path.join("{pcfHome}".format_map(rVars), "tmp", 'run_command_'+aRole)
-  runCmd = pexpect.spawn(
-    "ssh {ssh_user}@{aHost} {runCmdPath}".format(
-      aRole=aRole,
-      aHost=aHost,
-      runCmdPath=runCmdPath,
-      ssh_user=config['ssh_user']
-    ),
-    encoding='utf-8'
-  )
-  runCmd.logfile_read = logFile
-  while True :
-    pResult = runCmd.expect([
-      "Enter passphrase for key", pexpect.EOF, pexpect.TIMEOUT
-    ])
-    if pResult == 0 :
-      runCmd.sendline(secrets['ssh_pass'])
-    else : break
 
 def createFilesFor(aRole, rFiles, rVars, aHost, aDir, config, secrets, logFile) :
   logFile.write(f"creating files for {aRole} on {aHost}\n")
@@ -110,6 +120,7 @@ def createFilesFor(aRole, rFiles, rVars, aHost, aDir, config, secrets, logFile) 
 
 def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets, logFile) :
   logFile.write(f"creating global resources for {aRole} on {aHost}\n")
+  cmdTypes = {}
   rTasks = loadTasksFor(aRole)
   rVars  = {}
   mergeVars(rVars, gVars)
@@ -125,7 +136,28 @@ def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets, logFile)
     createFilesFor(aRole, rTasks['files'], rVars, aHost,  aDir, config, secrets, logFile)
 
   if 'commands' in rTasks :
-    createRunCommandFor(aRole, rTasks['commands'], rVars, aHost, aDir, config, secrets, logFile)
+    theTargetFile = aDir / "{pcfHome}".format_map(rVars) / 'tmp' / ('run_command_'+aRole)
+    cmdTypes['commands'] = str(theTargetFile)
+    createRunCommandFor(
+      aRole, rTasks['commands'], rVars, theTargetFile,
+      aHost, aDir, config, secrets, logFile
+    )
+
+  if 'start' in rTasks :
+    theTargetFile = aDir / "{pcfHome}".format_map(rVars) / 'bin' / ('start_'+aRole)
+    cmdTypes['start'] = str(theTargetFile)
+    createRunCommandFor(
+      aRole, rTasks['start'], rVars, theTargetFile,
+      aHost, aDir, config, secrets, logFile
+    )
+
+  if 'stop' in rTasks :
+    theTargetFile = aDir / "{pcfHome}".format_map(rVars) / 'bin' / ('stop_'+aRole)
+    cmdTypes['stop'] = str(theTargetFile)
+    createRunCommandFor(
+      aRole, rTasks['stop'], rVars, theTargetFile,
+      aHost, aDir, config, secrets, logFile
+    )
 
   if 'workers' in config :
     if aRole in config['workers'] :
@@ -134,33 +166,57 @@ def createLocalResourcesFor(aRole, gVars, aHost, aDir, config, secrets, logFile)
           rVars['aWorker'] = aWorker
           createFilesFor(aRole, rTasks['list'], rVars, aHost, aDir, config, secrets, logFile)
 
-def runCommandsFor(aRole, gVars, aHost, config, secrets, logFile) :
-  logFile.write(f"creating lobal resources for {aRole} on {aHost}\n")
-  rTasks = loadTasksFor(aRole)
-  rVars  = {}
-  mergeVars(rVars, gVars)
-  if 'vars' in rTasks :
-    mergeVars(rVars, rTasks['vars'])
-  if 'commands' in rTasks :
-    runCommandFor(aRole, rVars, aHost, config, secrets, logFile)
+  return cmdTypes
+
+def createStartStopRunCommandsForHost(cmdTypes, aHost, hDir, gVars, logFile) :
+  logFile.write(f"creating start/stop/run commands for {aHost}\n")
+  cmds = {}
+  for aWorker, someCmds in cmdTypes.items() :
+    for aCmd, aPath in someCmds.items() :
+      if aCmd not in cmds : cmds[aCmd] = []
+      cmds[aCmd].append('$HOME'+aPath.removeprefix(str(hDir)))
+  cmdTemplate = '''
+#!/bin/sh
+
+{% for aCmd in cmds %}
+{{ aCmd }}
+{% endfor %}
+  '''
+  defaultTargetFiles = {
+    'commands' : str(hDir / "{pcfHome}".format_map(gVars) / 'tmp' / 'run_commands'),
+    'start'    : str(hDir / "{pcfHome}".format_map(gVars) / 'bin' / 'startComputeFarm'),
+    'stop'     : str(hDir / "{pcfHome}".format_map(gVars) / 'bin' / 'stopComputeFarm')
+  }
+  targetFiles = {
+    'hDir'     : str(hDir),
+  }
+  for aCmd, someCmds in cmds.items() :
+    targetFiles[aCmd] = defaultTargetFiles[aCmd]
+    if aCmd == 'stop' : someCmds.reverse()
+    jinjaFile(cmdTemplate, targetFiles[aCmd], { 'cmds' : someCmds }, {}, logFile)
+    os.chmod(targetFiles[aCmd], 0o0755)
+
+  return targetFiles
 
 def createLocalResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
   logFile.write(f"creating local resources for {aHost}\n")
   hDir = tmpDir / aHost
   hDir.mkdir(parents=True, exist_ok=True)
+  cmdTypes = {}
 
   if aHost == platform.node() :
-    createLocalResourcesFor(
+    cmdTypes['taskManager'] = createLocalResourcesFor(
       'taskManager', gVars, aHost, hDir, config, secrets, logFile
     )
   if 'workers' in config :
-    createLocalResourcesFor(
+    cmdTypes['allWorkers'] = createLocalResourcesFor(
       'allWorkers', gVars, aHost, hDir, config, secrets, logFile
     )
     for aWorker in config['workers'] :
-      createLocalResourcesFor(
+      cmdTypes[aWorker] = createLocalResourcesFor(
         aWorker, gVars, aHost, hDir, config, secrets, logFile
       )
+  return createStartStopRunCommandsForHost(cmdTypes, aHost, hDir, gVars, logFile)
 
 def rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
     logFile.write(f"rsyncing resouces to {aHost}\n")
@@ -181,24 +237,24 @@ def rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
         rsyncCmd.sendline(secrets['ssh_pass'])
       else : break
 
-def runCommandsForHost(tmpDir, aHost, gVars, config, secrets, logFile) :
+def runCommandsForHost(aHost, cmdPaths, config, secrets, logFile) :
   logFile.write(f"running commands on {aHost}\n")
-  if aHost == platform.node() :
-    runCommandsFor('taskManager', gVars, aHost, config, secrets, logFile)
-  if 'workers' in config :
-    runCommandsFor('allWorkers', gVars, aHost, config, secrets, logFile)
-    for aWorker in config['workers'] :
-      runCommandsFor(aWorker, gVars, aHost, config, secrets, logFile)
-
-def setupAHost(tmpDir, aHost, gVars, config, secrets) :
-  print(f"Setting up host {aHost} in {tmpDir}/{aHost}")
-  timeNow = datetime.datetime.now()
-  logFilePath = Path("logs") / (aHost + '-' + timeNow.strftime('%Y-%m-%d_%H-%M-%s.%f'))
-  logFilePath.parent.mkdir(parents=True, exist_ok=True)
-  with open(logFilePath, "w") as logFile :
-    createLocalResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
-    rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
-    runCommandsForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+  runCmd = pexpect.spawn(
+    "ssh {ssh_user}@{aHost} {runCmdPath}".format(
+      aHost=aHost,
+      runCmdPath=cmdPaths['commands'].removeprefix(cmdPaths['hDir']+'/'),
+      ssh_user=config['ssh_user']
+    ),
+    encoding='utf-8'
+  )
+  runCmd.logfile_read = logFile
+  while True :
+    pResult = runCmd.expect([
+      "Enter passphrase for key", pexpect.EOF, pexpect.TIMEOUT
+    ])
+    if pResult == 0 :
+      runCmd.sendline(secrets['ssh_pass'])
+    else : break
 
 def isHostUp(aHost) :
   pingCmd = pexpect.spawn(f"ping {aHost}")
@@ -208,6 +264,21 @@ def isHostUp(aHost) :
   ])
   pingCmd.terminate(force=True)
   return pResult == 1
+
+def setupAHost(tmpDir, aHost, gVars, config, secrets) :
+  if not isHostUp(aHost) : return
+
+  print(f"Setting up host {aHost} in {tmpDir}/{aHost}")
+  timeNow = datetime.datetime.now()
+  logFilePath = Path("logs") / (aHost + '-' + timeNow.strftime('%Y-%m-%d_%H-%M-%s.%f') + '-setup')
+  logFilePath.parent.mkdir(parents=True, exist_ok=True)
+  with open(logFilePath, "w") as logFile :
+    cmdPaths = createLocalResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+    rsyncResourcesForHost(tmpDir, aHost, gVars, config, secrets, logFile)
+    if 'commands' in cmdPaths :
+      runCommandsForHost(aHost, cmdPaths, config, secrets, logFile)
+    logFile.write(f"Finished setting up host {aHost}")
+  print(f"Finished setting up host {aHost}")
 
 @click.command()
 @click.pass_context
@@ -242,6 +313,11 @@ def setup(ctx) :
     mergeVars(gVars, gTasks['vars'])
 
   hList = gConfig['hostList']
+  workThreads = []
   for aHost in hList :
-    if isHostUp(aHost) :
-      setupAHost(tmpDir, aHost, gVars, config[aHost], secrets[aHost])
+    workThreads.append(Thread(target=setupAHost, args=[
+      tmpDir, aHost, gVars, config[aHost], secrets[aHost]
+    ]))
+  for aThread in workThreads : aThread.start()
+  for aThread in workThreads : aThread.join()
+
