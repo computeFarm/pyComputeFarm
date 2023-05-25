c3824ce5bb52c051d8913bed1a43eca5b9b17467
Stephen Gaito <stephen@perceptisys.co.uk>
2023-05-25 17:14:47 +0100

    continued the refactoring of gcc workers

--- rcf/roleResources/gccARMWorker/__init__.py
+++
@@ -1,2 +0,0 @@
-"""This collection of resources descibes how to build a gcc native ARM
-Worker."""
--- rcf/roleResources/gccX86Worker/__init__.py
+++
@@ -1,2 +0,0 @@
-"""This collection of resources describes how to build a gcc native X86
-Worker."""
