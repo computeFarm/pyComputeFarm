099dc672fbbb260674a91aa4a171f0c1d81f47d6
Stephen Gaito <stephen@perceptisys.co.uk>
2023-05-25 17:13:36 +0100

    continued the refactoring of gcc workers

--- rcf/roleResources/gccARMCrossX86Worker/__init__.py
+++
@@ -1,2 +0,0 @@
-"""This collection of resources describes how to build a gcc ARM->X86
-cross compiling Worker."""
--- rcf/roleResources/gccX86CrossARMWorker/__init__.py
+++
@@ -1,2 +0,0 @@
-"""This collection of resources descibes how to build a gcc X86->ARM cross
-compiling Worker."""
